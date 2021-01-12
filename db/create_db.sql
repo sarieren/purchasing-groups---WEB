bfe589a2a3451225117f07da96190ffd9ce4c8d5
Sari erentreu <s0573153130@gmail.com>
2021-01-12 10:53:08 +0200

    create db

---
+++ db/create_db.sql
@@ -0,0 +1,42 @@
+
+
+ USE group_buy; 
+
+
+
+CREATE TABLE group(
+    id int NOT NULL PRIMARY KEY
+);
+
+-- CREATE database group_buy;
+
+--  CREATE TABLE user(
+--     user_name varchar(50) NOT NULL PRIMARY KEY,
+--     user_mail varchar(50),
+--     user_password varchar(50)
+--     );
+
+
+-- CREATE TABLE category(
+-- id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- name varchar(50) NOT NULL,
+-- );
+
+-- CREATE TABLE group(
+--     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     group_name varchar(50),
+--     item_name varchar(50) NOT NULL,
+--     max_price int NOT NULL,
+
+--     category_id int NOT NULL,
+--     manager varchar(50) NOT NULL,
+--     end_time DATETIME,
+
+--     FOREIGN KEY(category_id) REFERENCES category(id),
+--     FOREIGN KEY(manager) REFERENCES user(user_name)
+
+);
+
+
+
+
